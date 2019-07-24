09a0b874dc9bf41b202309094c81edecc8f47813
Majeeth <51352860+Majeeth@users.noreply.github.com>
2019-07-24 22:24:53 +0530

    Update p7.py

--- code/p7.py
+++ code/p7.py
@@ -1,15 +1,11 @@
-global count
-count=0  
-n1,n2=input().split()
-n1=int(n1)
-n2=int(n2)
-while(n1!=n2):
-    if(n1==2):
-        count+=1
-    for i in range(2,12):
-        if(n1%i==0 and n1!=i):
-            break
-    else:
-        count+=1
-    n1+=1
-print(count)
+global s1
+s2=input()
+s=list(s2)
+n=len(s)
+for i in range(0,n-1,2):
+    s1=s[i]
+    s[i]=s[i+1]
+    s[i+1]=s1
+s3=''.join(s)
+print(s3)
+
