4a26a5641e099a640c90b61e5138d093b469aab0
Majeeth <51352860+Majeeth@users.noreply.github.com>
2019-07-26 20:41:38 +0530

    Update p19.py

--- code/p19.py
+++ code/p19.py
@@ -1,8 +1,14 @@
 x1=[]
+x2=[]
 k=int(input())
 for i in range(0,k):
-    x=([str(i) for i in input().split()])
+    x=([int(i) for i in input().split()])
     x1.extend(x)
 x1.sort()
-s=' '.join(x1)
+x1
+for i in x1:
+    c=str(i)
+    x2.append(c)
+x2
+s=' '.join(x2)
 print(s)
